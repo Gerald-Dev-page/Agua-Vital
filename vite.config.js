395c4a721d888029cc9469b3f7ea29ffd1ad3fd7
Gerald <lorenzomuoz321@gmail.com>
2026-04-10 16:11:39 -0300

    1.3

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // El base debe ser exactamente el nombre de tu repositorio entre barras
-  base: '/Agua-Vital/', 
+  base: '/', 
 })
